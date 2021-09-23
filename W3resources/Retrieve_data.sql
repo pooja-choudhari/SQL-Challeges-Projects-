d2e263c5a53f1e0e0e54998b066431088d3b29cf
Pooja Choudhari <poojachoudhari0792@gmail.com>
2021-09-22 19:09:05 -0700

    USing union and NOT In operator

--- W3resources/Retrieve_data.sql
+++ W3resources/Retrieve_data.sql
@@ -132,3 +132,18 @@ WHERE year >= 1972 AND WINNER IN ('Menachem Begin', 'Yitzhak Rabin');
 SELECT *
 FROM nobel_win
 WHERE WINNER LIKE 'Louis%';
+
+15. From the following table, write a SQL query to combine the winners in Physics, 1970 and in Economics, 1971. Return year, subject, winner, country, and category.
+SELECT *
+FROM nobel_win
+WHERE SUBJECT = 'Physics' AND YEAR = 1970
+UNION (SELECT *
+       FROM nobel_win
+      WHERE SUBJECT = 'Economics' AND YEAR = 1971  );
+
+16. From the following table, write a SQL query to find the Nobel Prize winners in 1970 excluding the subjects Physiology and Economics. Return year, subject, winner, country, and category.
+SELECT *
+FROM nobel_win
+WHERE SUBJECT NOT IN ('Physiology', 'Economics' );
+
+      
