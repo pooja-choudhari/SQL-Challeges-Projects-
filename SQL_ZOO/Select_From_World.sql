2deafec57c235a75fb361e5d97f464e5cf80dc05
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-02-09 15:56:48 -0800

    XOR operator

--- SQL_ZOO/Select_From_World.sql
+++ SQL_ZOO/Select_From_World.sql
@@ -32,3 +32,17 @@ WHERE name LIKE 'United%';
 SELECT name
 FROM world
 WHERE name LIKE 'United%';
+
+8.Show the countries that are big by area or big by population. Show name, population and area.
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 OR population >= 250000000;
+
+9.Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 XOR population > 250000000;
+
+XOR operator in MYSQL - https://www.w3resource.com/mysql/logical-operators/xor-operator.php
+
+10.
