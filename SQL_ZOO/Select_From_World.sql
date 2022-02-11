bfcb5bbc18ca8ba26e9c561449445f709983f98a
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-02-11 15:11:20 -0800

    Length funtion

--- SQL_ZOO/Select_From_World.sql
+++ SQL_ZOO/Select_From_World.sql
@@ -52,3 +52,13 @@ FROM world
 WHERE continent = 'South America';
 
 ROUND FUNCTION - https://learnsql.com/cookbook/how-to-round-numbers-in-sql/
+
+11.Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+SELECT name, ROUND(GDP/population, -3) AS PerCapitaGDP
+FROM world
+WHERE GDP >= 1000000000000;
+
+12.Show the name and capital where the name and the capital have the same number of characters.
+SELECT name, capital
+FROM world
+WHERE LENGTH(name) = LENGTH(capital);
