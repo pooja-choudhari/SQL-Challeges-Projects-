2ff2bb54d89b5ad95041b11502094ca9572e7b34
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-02-11 16:39:44 -0800

    LEFT function

--- SQL_ZOO/Select_From_World.sql
+++ SQL_ZOO/Select_From_World.sql
@@ -62,3 +62,8 @@ WHERE GDP >= 1000000000000;
 SELECT name, capital
 FROM world
 WHERE LENGTH(name) = LENGTH(capital);
+
+13.Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+SELECT name, capital
+FROM world
+WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital;
