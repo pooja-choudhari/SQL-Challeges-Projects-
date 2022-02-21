5cb1bc511006e6496e746c284045b0fcf442d0d6
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-02-21 11:49:56 -0800

    the name and the per capita GDP

--- SQL_ZOO/Select_From_World.sql
+++ SQL_ZOO/Select_From_World.sql
@@ -8,3 +8,6 @@ FROM world
 WHERE population >= 200000000;
 
 3.Give the name and the per capita GDP for those countries with a population of at least 200 million.
+SELECT name, GDP/population AS per_capita
+FROM world
+WHERE population >= 200000000;
