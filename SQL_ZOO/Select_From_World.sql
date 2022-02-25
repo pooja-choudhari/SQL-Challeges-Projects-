cd1323d9aa39dc1dd0dda17264049fccdaf5d279
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-02-24 17:50:34 -0800

    countries that are big by area or big by population

--- SQL_ZOO/Select_From_World.sql
+++ SQL_ZOO/Select_From_World.sql
@@ -26,3 +26,8 @@ WHERE name IN ('France', 'Germany', 'Italy');
 SELECT name
 FROM world
 WHERE name LIKE '%United%';
+
+7.Show the countries that are big by area or big by population. Show name, population and area.
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 OR population > 250000000;
