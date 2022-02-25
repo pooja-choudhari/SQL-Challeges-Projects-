496f8b64e32ade7a821a01dd274aea2d39423d7c
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-02-24 17:50:02 -0800

    a name that includes the word 'United'

--- SQL_ZOO/Select_From_World.sql
+++ SQL_ZOO/Select_From_World.sql
@@ -21,3 +21,8 @@ WHERE continent = 'South America';
 SELECT name, population
 FROM world
 WHERE name IN ('France', 'Germany', 'Italy');
+
+6.Show the countries which have a name that includes the word 'United'
+SELECT name
+FROM world
+WHERE name LIKE '%United%';
