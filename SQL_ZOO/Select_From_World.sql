8d72033f43cc682922c5624027f42ad336668033
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-02-28 20:54:23 -0800

    country that has all the vowels and no spaces in its name

--- SQL_ZOO/Select_From_World.sql
+++ SQL_ZOO/Select_From_World.sql
@@ -55,3 +55,13 @@ WHERE LENGTH(name) = LENGTH(capital);
 SELECT name, capital
 FROM world
 WHERE name <> capital AND LEFT(name,1) = LEFT(capital,1);
+
+13. Find the country that has all the vowels and no spaces in its name.
+SELECT name
+FROM world
+WHERE name LIKE '%a%'
+AND name LIKE '%e%'
+AND name LIKE '%i%'
+AND name LIKE '%o%'
+AND name LIKE '%u%'
+AND name NOT LIKE '% %';
