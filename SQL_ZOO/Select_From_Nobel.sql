5b20e3f2f97efc091b8bc73a940cd11893bc961b
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-03-06 21:40:21 -0800

    name of Physics winners for 1980 together with the Chemistry winners for 1984.

--- SQL_ZOO/Select_From_Nobel.sql
+++ SQL_ZOO/Select_From_Nobel.sql
@@ -32,3 +32,8 @@ WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack
 SELECT winner
 FROM nobel
 WHERE winner LIKE 'John%';
+
+8.Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
+SELECT *
+FROM nobel
+WHERE subject = 'Physics' AND yr = 1980 OR subject = 'Chemistry' AND yr = 1984;
