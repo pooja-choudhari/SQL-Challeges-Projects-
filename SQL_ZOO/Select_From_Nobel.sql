81ef52fa5739d949ab94065eb2a31913e4b3d889
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-03-11 16:37:28 -0800

    Order BY

--- SQL_ZOO/Select_From_Nobel.sql
+++ SQL_ZOO/Select_From_Nobel.sql
@@ -52,4 +52,11 @@ WHERE (subject='Medicine' and yr <1910) OR
 11.Find all details of the prize won by PETER GRÜNBERG
 SELECT *
 FROM nobel
-WHERE winner = 'PETER GRÜNBERG';      
+WHERE winner = 'PETER GRÜNBERG';
+
+12.Find all details of the prize won by EUGENE O'NEILL
+SELECT *
+FROM nobel
+WHERE winner = 'EUGENE O''NEILL';
+
+13.List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.  
