abeb1f04ab1f9ab0cfae2ea5b962018b554fdc1d
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-03-21 17:50:33 -0700

    continents that have a total population of at least 100 million

--- SQL_ZOO/Sum_And_Count.sql
+++ SQL_ZOO/Sum_And_Count.sql
@@ -31,3 +31,9 @@ SELECT continent, COUNT(name)
 FROM world
 WHERE population >= 10000000
 GROUP BY continent;
+
+8.List the continents that have a total population of at least 100 million.
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) >= 100000000;
