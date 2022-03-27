2dd3af05cbcb9bcd19cfd0446ac12bbc7e2b1a18
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-03-26 18:23:28 -0700

    the dates of the matches and the name of the team

--- SQL_ZOO/Join.sql
+++ SQL_ZOO/Join.sql
@@ -27,3 +27,7 @@ JOIN eteam ON goal.teamid = eteam.id
 WHERE gtime <= 10;
 
 6. List the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
+SELECT mdate, teamname
+FROM game
+JOIN eteam ON game.team1 = eteam.id
+WHERE coach = 'Fernando Santos';
