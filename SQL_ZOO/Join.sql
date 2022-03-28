ac51499388173813a0b13e2410f4865c83e96853
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-03-28 14:11:03 -0700

    show the name of all players who scored a goal against Germany

--- SQL_ZOO/Join.sql
+++ SQL_ZOO/Join.sql
@@ -37,3 +37,9 @@ SELECT player
 FROM goal
 JOIN game ON goal.matchid = game.id
 WHERE stadium = 'National Stadium, Warsaw';
+
+8.Instead show the name of all players who scored a goal against Germany.
+SELECT DISTINCT(player)
+FROM game
+JOIN goal ON matchid = id
+WHERE teamid != 'GER' AND (team1 = 'GER' OR team2 = 'GER');
