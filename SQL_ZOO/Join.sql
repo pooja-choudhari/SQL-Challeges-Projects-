83645b359907814f77e259c9e7a475edb07eba95
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-03-29 22:23:52 -0700

    teamname and the total number of goals scored

--- SQL_ZOO/Join.sql
+++ SQL_ZOO/Join.sql
@@ -43,3 +43,9 @@ SELECT DISTINCT(player)
 FROM game
 JOIN goal ON matchid = id
 WHERE teamid != 'GER' AND (team1 = 'GER' OR team2 = 'GER');
+
+9.Show teamname and the total number of goals scored.
+SELECT teamname, COUNT(gtime)
+FROM eteam
+JOIN goal ON id=teamid
+GROUP BY teamname;
