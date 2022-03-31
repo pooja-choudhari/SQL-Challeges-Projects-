574f885e0abc16ad580f67a8854b599e34ade4ae
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-03-30 21:29:17 -0700

    show the matchid, date and the number of goals scored.

--- SQL_ZOO/Join.sql
+++ SQL_ZOO/Join.sql
@@ -57,3 +57,7 @@ JOIN goal ON goal.matchid = game.id
 GROUP BY stadium;
 
 11.For every match involving 'POL', show the matchid, date and the number of goals scored.
+SELECT matchid, mdate, COUNT(gtime)
+FROM game JOIN goal ON goal.matchid = game.id
+WHERE team1 = 'POL' OR team2 = 'POL'
+GROUP BY matchid, mdate;
