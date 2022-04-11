f5beaba66a0033baa3f2906d57d19df53090c165
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-04-10 19:31:29 -0700

    Star Trek in the title

--- SQL_ZOO/MoreJOIN.sql
+++ SQL_ZOO/MoreJOIN.sql
@@ -7,3 +7,5 @@ WHERE yr = 1962;
 SELECT yr
 FROM movie
 WHERE title = 'Citizen Kane';
+
+3.List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
