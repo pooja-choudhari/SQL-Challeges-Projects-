f4d6bbd18924a81f564dfc778131738a0583ca7b
Pooja Choudhari <poojachoudhari0792@gmail.com>
2022-04-10 19:31:52 -0700

    List all of the Star Trek movies

--- SQL_ZOO/MoreJOIN.sql
+++ SQL_ZOO/MoreJOIN.sql
@@ -9,3 +9,7 @@ FROM movie
 WHERE title = 'Citizen Kane';
 
 3.List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE '%Star%Trek%'
+ORDER by yr;
